f66330f5ae63e2c306fe69173abff897894b71e5
ktsin <43731609+ktsin@users.noreply.github.com>
2021-04-22 10:07:49 +0300

    backup_10072204

--- DAL/Migrations/20210422070457_Start.Designer.cs
+++ DAL/Migrations/20210422070457_Start.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using DAL.Repository.EFCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace DAL.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210422070457_Start")]
+    partial class Start
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
